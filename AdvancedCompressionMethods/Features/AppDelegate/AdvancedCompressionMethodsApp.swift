d112e5819be91ad69345a86f986da5dae5ec2c99
Vlad Stanescu <vladstanescu94@gmail.com>
2021-03-07 18:56:34 +0200

    initial setup

---
+++ AdvancedCompressionMethods/Features/AppDelegate/AdvancedCompressionMethodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedCompressionMethodsApp.swift
+//  AdvancedCompressionMethods
+//
+//  Created by Vlad Stanescu on 07.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedCompressionMethodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ AdvancedCompressionMethods/Features/Home/Views/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  AdvancedCompressionMethods
+//
+//  Created by Vlad Stanescu on 07.03.2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
