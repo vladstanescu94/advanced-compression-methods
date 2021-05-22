0e24742b42b79e752c1ade77d9084d3c7c86f61e
Vlad Stanescu <vladstanescu94@gmail.com>
2021-05-22 18:19:37 +0300

    Add files for near lossless encoder and decoder

---
+++ AdvancedCompressionMethods/Features/NearLossless/Decoder/NearLosslessDecoder.swift
@@ -0,0 +1,12 @@
+//
+//  NearLosslessDecoder.swift
+//  AdvancedCompressionMethods
+//
+//  Created by Vlad Stanescu on 22.05.2021.
+//
+
+import Foundation
+
+class NearLosslessDecoder {
+    init() { }
+}
---
+++ AdvancedCompressionMethods/Features/NearLossless/Encoder/NearLosslessEncoder.swift
@@ -0,0 +1,12 @@
+//
+//  NearLosslessEncoder.swift
+//  AdvancedCompressionMethods
+//
+//  Created by Vlad Stanescu on 22.05.2021.
+//
+
+import Foundation
+
+class NearLosslessEncoder {
+    init() { }
+}
